e9063488d094d280f5c8d4d5907fb3660dbc2dcc
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-11-15 19:45:13 -0300

    Update controller

--- src/controller/controller_bot.py
+++ src/controller/controller_bot.py
@@ -1,6 +1,8 @@
 
+# ===== IMPORTACOES =====
 #* Importacoes
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import pyautogui as p
@@ -9,12 +11,20 @@ import logging
 #* Importacao das funcoes
 from src.model.login_amazon import login_amazon
 
+# ===== CONFIGURACOES GOOGLE CHROME E SELENIUM =====
 #* Baixar versao correta do chromedrive e armazenar em cache
 service = ChromeService(executable_path=ChromeDriverManager().install()) #* instalar chromedriver
 
-#* Inicar instancia do chromedriver
-navegador = webdriver.Chrome(service=service)
+# Configurar options para abrir o chrome no perfil do usuario
+options = webdriver.ChromeOptions()
+# options.add_argument(r'--user-data-dir=C:\Users\Usuário\AppData\Local\Google\Chrome\User Data') #* aponta para a pasta user data principal
+# options.add_argument(r'--profile-directory=Profile 2') #* definicao do perfil a ser utilizado
+options.add_experimental_option("detach", True) #* evita que o chrome feche sozinho
 
+#* Iniciar instancia do chromedrive
+driver = webdriver.Chrome(service=service, options=options)
+
+# ===== CREDENCIAIS DE ACESSO =====
 #* Ler arquivo de credenciais e salvar email e senha
 arquivo_credenciais = open(r'C:\Users\Usuário\credenciais\login_amazon.txt', 'r')
 credenciais = arquivo_credenciais.readlines()
@@ -23,6 +33,7 @@ arquivo_credenciais.close()
 email = credenciais[0][:-1]
 senha = credenciais[1]
 
+# ===== CONFIGURACOES DO LOGGING
 #* Configuracao de logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -31,17 +42,17 @@ logging.basicConfig(
     filename='mylog.log'
 )
 
+# ===== INICIO DO BOT =====
 print('===== Bot de Coleta de Produtos da Amazon =====')
 logging.info('===== Bot de Coleta de Produtos da Amazon =====')
 
 #* Abrir site da amazon
-print('Abrindo site da Amazon, aguarde...')
-navegador.get('https://www.amazon.com.br')
-navegador.maximize_window()
+print('Abrindo site da Amazon, aguarde')
+driver.get('https://www.amazon.com.br')
+driver.maximize_window()
 
 #* Variavel para definir tempo de espera para carregamento de elementos
 tempo_espera = 100
 
 #* Execucao das funcoes
-login_amazon(navegador, tempo_espera, email, senha)
-p.alert('oi')
+login_amazon(driver, tempo_espera, email, senha)
